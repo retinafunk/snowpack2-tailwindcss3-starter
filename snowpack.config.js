193a8ad7d11ad5d26e1ef7ff9786f42c6a16cc4c
retinafunk <retinafunk@gmail.com>
2021-07-11 15:24:25 +0200

    Config snowpack to use tailwind

---
+++ snowpack.config.js
@@ -0,0 +1,13 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  buildOptions: {
+    baseUrl: '',
+  },
+  plugins: [
+    '@snowpack/plugin-postcss',
+  ]
+}
